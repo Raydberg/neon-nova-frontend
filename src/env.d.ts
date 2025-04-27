90a0480bc1559aebd57f04fbedc4f4f45af58300
Raydberg Gabriel <raydbergg@gmail.com>
2025-04-27 15:08:12 -0500

    fix(env): correct property name from NG_APP_CLIENT_URL to NG_APP_API_CLIENT_URL in Env interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,7 @@ declare interface Env {
 
   NG_APP_API_URL: string;
   // NG_APP_GOOGLE_AUTH_CALLBACK:string
-  NG_APP_CLIENT_URL:string
+  NG_APP_API_CLIENT_URL:string
 }
 
 declare interface ImportMeta {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: import.meta.env.NG_APP_API_URL,
-  apiClientUrl:import.meta.env.NG_APP_CLIENT_URL
+  apiClientUrl:import.meta.env.NG_APP_API_CLIENT_URL
 };
